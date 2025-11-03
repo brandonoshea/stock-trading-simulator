9e4810410f5572ee59e651687a5e9e2b9b8f0486
brandonoshea <bfoshea@clemson.edu>
2025-11-03 16:55:22 -0500

    robinhood logo

---
+++ robinhood-clone/src/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Logo from './robinhood.svg'
+function Header() {
+  return (
+    <div className = "headerwrapper">
+        {/* logo */}
+        <div className = "header_logo">
+            <img src = {Logo} width = {25} />
+        </div>
+        {/* Search */}
+        {/* Menu Items */}
+    </div>
+  )
+}
+
+export default Header
