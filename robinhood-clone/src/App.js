66dbb4c3d3e8a744e5a772fc23c127d0f7754dc9
brandonoshea <bfoshea@clemson.edu>
2025-11-14 12:33:33 -0500

    menu items css changes

--- robinhood-clone/src/App.js
+++ robinhood-clone/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './Header'
-
+import Newsfeed from './Newsfeed'
+import Stats from './Stats'
 
 function App() {
   return (
@@ -11,7 +12,11 @@ function App() {
         <Header />
       </div>
       <div className = "app_body">
-
+        <div className="app__container">
+          <Newsfeed />
+          {/*Stats*/}
+          <Stats />
+        </div>
       </div>
     </div>
   );
---
+++ robinhood-clone/src/Stats.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Stats() {
+  return (
+    <div className="stats">
+        
+      
+    </div>
+  )
+}
+
+export default Stats
